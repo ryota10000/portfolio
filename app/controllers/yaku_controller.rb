c397d66d7523479df0bce920c55dcfbaebdfd3b4
ryota10000 <shimizu.199812@gmail.com>
2024-10-18 15:07:26 +0900

    麻雀点数計算

---
+++ app/controllers/yaku_controller.rb
@@ -0,0 +1,19 @@
+class YakuController < ApplicationController 
+  def index
+    @yaku_values = {
+      'リーチ' => { han: 1, fu: 0 },
+      'ツモ' => { han: 1, fu: 20 },
+      '平和' => { han: 1, fu: 0 },
+      '一発' => { han: 1, fu: 20 },
+      # 他の役も追加できます
+    }
+
+    # 翻、符、得点を一緒に定義する
+    @yaku_score = {
+      'han:1,fu:0' => 1000,   # 翻1、符0の場合の得点
+      'han:1,fu:20' => 2000,  # 翻1、符20の場合の得点
+      'han:2,fu:0' => 4000,   # 翻2、符0の場合の得点
+      'han:2,fu:20' => 8000,   # 翻2、符20の場合の得点
+    }
+  end
+end
