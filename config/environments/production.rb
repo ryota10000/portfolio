70c5876a42cbcf90a943c71551c235f7c58dcbba
ryota10000 <shimizu.199812@gmail.com>
2025-03-31 18:37:28 +0900

    麻雀点数計算

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,4 +99,5 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.hosts << '3ninma-zyan.com'
 end
